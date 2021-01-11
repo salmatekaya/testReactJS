a1c7c2f12fe642eb86b68bfc45e20ce96c4672a3
salmatekaya <you@example.com>
2021-01-11 21:01:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import Body from './components/Body';
+function App() {
+  return ( <div style={{backgroundColor:'black',height:'800px',width:'100%'}}>
+    <Body/>
+    </div>
+  );
+}
+
+export default App;
